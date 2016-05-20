3a0ac30676a07278606eead08c0646b4f5d0ea9e
Joao Poupino <joaopfp@brpx.com>
2016-05-20 13:42:08 +0100

    Fix imports

--- redis_protocol/__init__.py
+++ redis_protocol/__init__.py
@@ -4,6 +4,6 @@
 __author__ = 'Young King'
 __email__ = 'yanckin@gmail.com'
 __version__ = '0.0.1'
-from .protocol import encode, decode, parse_stream
+from .protocol import encode, decode, decode_stream
 
 __all__ = ["encode", "decode"]
